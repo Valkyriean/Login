af045809bf5019033e267ff97ad26e0fa0f64700
JoshMYan <yanboxiao1999@126.com>
2016-10-24 10:44:59 +0800

    fixes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -3,8 +3,7 @@ var inite=function(){
   console.log("Hellow world");
   $("#bot" ).click(function() {
     alert( "Yes you did it" );
-      $(".bot").css("color","black");
-      $(".bot").css("background-color","green");
+      $(".bot").css("background-color","D5FFFD");
       $(".block1").css("color","red");
     // $("body").css("background-color","red");
   });
