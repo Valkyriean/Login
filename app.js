91dea4c524fbc77ef707e2b5d44419b8cf6f192b
Valkyriean <phantomgale@hotmail.com>
2016-10-25 09:56:40 +0800

    gif
    干好了

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ var app = express();
 
 app.use(express.static('public'));
 app.use('/api',api);
-app.use(express.json());
-app.use(express.bodyParser());
+app.use(bodyParser.json());
 
 
 app.post('/', function (req, res) {
@@ -22,32 +21,7 @@ app.post('/', function (req, res) {
 })
 
 
-app.post('/', function (req, res) {
-    var username = req.body.username;
-    var password = req.body.password;
-    res.set('Content-Type', 'application/json; charset=utf-8');
-    if (username=="alex" && password=='alex') {
-        //能正确解析 json 格式的post参数
-        res.send({"status": "success"});
-    } else {
-        //不能正确解析json 格式的post参数
-        var body = '', jsonStr;
-        req.on('data', function (chunk) {
-            body += chunk; //读取参数流转化为字符串
-        });
-        req.on('end', function () {
-            //读取参数流结束后将转化的body字符串解析成 JSON 格式
-            try {
-                jsonStr = JSON.parse(body);
-            } catch (err) {
-                jsonStr = null;
-            }
-            if(username=="alex" && password){
-                jsonStr ? res.send({"status":"success"}) : res.send({"status":"error"});
-            }
-        });
-    }
-});
+
 
 // app.post('/', function(req, res) {
 //     ans=req.getJSON('')
--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,35 @@
  */
 var express = require('express');
 var router = express.Router();
-// var app = express();
+var app = express();
 
 
-router.get('/login', function (req, res) {
-    res.send('Welcome');
+app.post('/login', function (req, res) {
+    var username = req.body.username;
+    var password = req.body.password;
+    if (username=="alex" && password=='alex') {
+        //能正确解析 json 格式的post参数
+        res.json({"status": "success"});
+    } else {
+        //不能正确解析json 格式的post参数
+        var body = '', jsonStr;
+        req.on('data', function (chunk) {
+            body += chunk; //读取参数流转化为字符串
+        });
+        req.on('end', function () {
+            //读取参数流结束后将转化的body字符串解析成 JSON 格式
+            try {
+                jsonStr = JSON.parse(body);
+            } catch (err) {
+                jsonStr = null;
+            }
+            if(username=="alex" && password){
+                jsonStr ? res.send({"status":"success"}) : res.send({"status":"error"});
+            }
+        });
+    }
 });
+
 
 
 module.exports = router;
