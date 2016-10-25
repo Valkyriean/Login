3213da53a870ee025fbde587a7938c5b9665c6f9
Valkyriean <phantomgale@hotmail.com>
2016-10-25 09:35:28 +0800

    1
    111

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,12 +4,7 @@ var inite=function(){
   $("#bot" ).click(function() {
     alert( "Yes you did it" );
       $(".bot").css("background-color","D5FFFD");
-<<<<<<< HEAD
-      $(".block1").css("color","red");
-      // $.post();//发出用户名密码
-=======
       $(".load").css("color","red");
->>>>>>> origin/master
     // $("body").css("background-color","red");
   });
 }
