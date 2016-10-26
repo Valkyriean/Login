7375e5f356999da7fadb613fd49e414086d67eef
Alex Li <phantomgale@hotmail.com>
2016-10-26 14:33:35 +0800

    add $.post

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,24 +1,28 @@
 var url = "http://localhost:3000/api/login";
 
 var init = function() {
-  $("#Button").click(function(e) {
-    e.preventDefault();
-    var text = document.getElementById("EnterText").value;
-    var password = document.getElementById("EnterPassword").value;
+    $("#Button").click(function(e) {
+        e.preventDefault();
+        var text = document.getElementById("EnterText").value;
+        var password = document.getElementById("EnterPassword").value;
 
 
-    var settings = {
-      "async": true,
-      "crossDomain": true,
-      "url": url,
-      "method": "POST",
-      "headers": {
-        "content-type": "application/json",
-        //"cache-control": "no-cache",
-        //"postman-token": "111a5e94-6cf4-0e6c-915a-84d49505e7f0"
-      },
-      "data": "{\"username\":\""+text+"\",\"password\":\""+password+"\"}"
-    }
+        // $.post(
+        //   url,
+        //     "{\"username\":\""+text+"\",\"password\":\""+password+"\"}",
+        //   function(response){
+        //       alert(text+password);
+        //       alert("Login "+response.status);
+        //   }, "json"
+        // );
+        var settings = {
+            "url": url,
+            "method": "POST",
+            "headers": {
+            "content-type": "application/json",
+        },
+        "data": "{\"username\":\""+text+"\",\"password\":\""+password+"\"}"
+        }
 
     $.ajax(settings).done(function (response) {
       console.log(response.status);
--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,17 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/login', function (req, res) {
-    //debug
-    console.log("got it");
-
+    console.log("connected");
     var username = req.body.username;
     var password = req.body.password;
+    console.log(username);
+    console.log(password);
     if (username=="alex" && password=='alex') {
-        //能正确解析 json 格式的post参数
-        console.log(username);
-        console.log(password);
         res.json({"status": "success"});
         console.log("success");
     } else {
-        console.log(username);
-        console.log(password);
-        res.send({"status":"error"});
-        console.log("error");
+        res.send({"status":"failed"});
+        console.log("failed");
     }
 });
 
