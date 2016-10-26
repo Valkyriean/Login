4262f41bdc23589bc6b574d5a53fc66f76de6968
JoshMYan <yanboxiao1999@126.com>
2016-10-26 11:49:54 +0800

    Josh

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,34 @@
+var url = "http://localhost:3000/api/login";
 
-var inite=function(){
-  console.log("Hellow world");
-
-  $("#bot" ).click(function() {
-    alert( "Yes you did it" );
-      $(".bot").css("background-color","D5FFFD");
-      $(".load").css("color","red");
-    // $("body").css("background-color","red");
-  });
+function test() {
+  alert("Hello!")
 }
 
-$(document).ready(inite);
+var init = function() {
+  $("#Button").click(function(e) {
+    e.preventDefault();
+    var text = document.getElementById("EnterText").value;
+    var password = document.getElementById("EnterPassword").value;
+
+
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": url,
+      "method": "POST",
+      "headers": {
+        "content-type": "application/json",
+        //"cache-control": "no-cache",
+        //"postman-token": "111a5e94-6cf4-0e6c-915a-84d49505e7f0"
+      },
+      "data": "{\"username\":\""+text+"\",\"password\":\""+password+"\"}"
+    }
+
+    $.ajax(settings).done(function (response) {
+      console.log(response.status);
+      alert("Login "+response.status)
+    });
+  });
+};
+
+$(document).ready(init);
